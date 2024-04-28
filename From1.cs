cb0905d2d3df73a91629f8f014f136d39f741c8f
veighn <132882569+veighn@users.noreply.github.com>
2024-04-29 02:03:23 +0400

    From1.cs

---
+++ From1.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc_Test_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                int c; // The variable 'c' will hold the result of the calculations.
+
+                if (radioButton1.Checked)
+                {
+                    c = a + b;
+                    label4.Text = "Add = " + c.ToString(); // Add first and second number.
+                }
+                else if (radioButton2.Checked)
+                {
+                    c = a - b;
+                    label4.Text = "Subtract = " + c.ToString(); // Subtract second from first number.
+                }
+                else if (radioButton3.Checked)
+                {
+                    c = a * b;
+                    label4.Text = "Multiply = " + c.ToString(); // Multiply first and second number.
+                }
+                else if (radioButton4.Checked)
+                {
+                    if (b != 0) // Check for division by zero.
+                    {
+                        c = a / b;
+                        label4.Text = "Divide = " + c.ToString(); // Divide first by second number.
+                    }
+                    else
+                    {
+                        label4.Text = "DIVIDE BY ZERO ERROR"; // Handle division by zero error.
+                    }
+                }
+                else if (radioButton5.Checked)
+                {
+                    if (b != 0) // Check for division by zero.
+                    {
+                        c = a % b;
+                        label4.Text = "Mod = " + c.ToString(); // Mod first number and second number.
+                    }
+                    else
+                    {
+                        label4.Text = "MODULO BY ZERO ERROR"; // Handle modulo by zero error.
+                    }
+                }
+                else
+                {
+                    label4.Text = "Please select an operation."; // Prompt the user to select an operation.
+                }
+            }
+            catch (FormatException)
+            {
+                label4.Text = "Invalid input. Please enter valid numbers."; // Handle invalid input format.
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label4.Text = "";
+
+        }
+
+       
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Black;
+            button1.ForeColor = Color.White;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.White;
+            button1.ForeColor = Color.Black;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Black;
+            button2.ForeColor = Color.White;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Black;
+            button2.ForeColor = Color.White;
+        }
+
+    
+        
+    }
+}
